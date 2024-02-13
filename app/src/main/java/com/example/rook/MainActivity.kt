a46a87f9bf3c958c4820b3cd8d7a9cb5559cd3ce
Shubu Aryal <scaryal06@gmail.com>
2024-02-13 11:21:54 -0800

    2/13/12 proj#b

--- app/src/main/java/com/example/rook/MainActivity.kt
+++ app/src/main/java/com/example/rook/MainActivity.kt
@@ -3,6 +3,12 @@ package com.example.rook
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+
+/**
+ * NUX said we made a mistake using kotlin instead of java but its OK for now
+ * we will have to copy and paste our proj into a new one thats java
+ */
+
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
